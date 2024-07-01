0fb90f42ff2ed2eaee03fb168468b58cb60db4cc
Ashik S. Nair <42920676+ASHIK-S-NAIR@users.noreply.github.com>
2024-07-01 17:26:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const PORT = 8000 || process.env;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to get request" });
+});
+
+app.post("/", (req, res) => {
+  console.log("body: ", req.body);
+  res.json({ statusCode: 200 });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
