dbbd068b517ea4cba10eddfbb7c4c5b7325807a4
Ashik S. Nair <42920676+ASHIK-S-NAIR@users.noreply.github.com>
2024-07-02 09:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log("body: ", req.body);
-  res.json({ statusCode: 200 });
+  res.send("Task timed out");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
