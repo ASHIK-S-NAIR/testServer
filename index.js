d6714fb54f20936ba4afc81eeb5b9bf31495cdaa
Ashik S. Nair <42920676+ASHIK-S-NAIR@users.noreply.github.com>
2024-07-04 22:16:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log("body: ", req.body);
-  res.json({msg: "Everything is ok"});
+  res.json({status: 201});
 });
 
 app.post("/log", (req, res) => {
