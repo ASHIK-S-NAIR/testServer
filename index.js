9f76d21d3ddc2c55d2fb3b7158127deb63614a14
Ashik S. Nair <42920676+ASHIK-S-NAIR@users.noreply.github.com>
2024-07-04 18:39:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log("body: ", req.body);
-  res.json({msg: "Task timed out"});
+  res.json({msg: "Everything is ok"});
 });
 
 app.post("/log", (req, res) => {
