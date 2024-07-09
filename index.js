ad0d621555b42447f7756255f44914957a11a757
Ashik S. Nair <42920676+ASHIK-S-NAIR@users.noreply.github.com>
2024-07-09 11:18:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to get request" });
+  return res.json({ msg: "Welcome to get request" });
 });
 
 app.post("/", (req, res) => {
   console.log("body: ", req.body);
-  res.json({status: 201});
+  return res.json({status: 201});
 });
 
 app.post("/log", (req, res) => {
   console.log("log: ", req.body);
-  res.json({msg: "log received"});
+  return res.json({msg: "log received"});
 });
 
 app.post("/ebApi", (req, res) => {
   console.log("data at EB: ", req.body);
-  res.json(req.body);
+  return res.json(req.body);
 });
   
 
